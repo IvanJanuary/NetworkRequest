ad1b20934281775ccc8469b54c825ad339d641dc
Ivan <>
2023-08-14 15:03:02 +0300

    Use GitHub API for search repositories
    Add Search Bar for user input to search repositories
    Add TableView to display Search Results
    Fetch Repositories from GitHub using url request

---
+++ NetworkRequest/NetworkRequest/ViewController.swift
@@ -0,0 +1,86 @@
+//
+//  ViewController.swift
+//  NetworkRequest
+//
+//  Created by Ivan on 02.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UISearchBarDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    private var repositories: [Repository] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        searchBar.delegate = self
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    @objc func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let searchText = searchBar.text else { return }
+        searchRepositories(withQuery: searchText)
+    }
+    
+    func searchRepositories(withQuery query: String) {
+        guard !query.isEmpty else {
+            repositories.removeAll()
+            tableView.reloadData()
+            return
+        }
+        
+        let api = "https://api.github.com/search/repositories?q="
+        guard let url = URL(string: api) else { fatalError("some Error") }
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            if let error = error {
+                print("Ошибка запроса: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let data = data else { return }
+            
+            do {
+                let decoder = JSONDecoder()
+                let searchResult = try decoder.decode(SearchResult.self, from: data)
+                DispatchQueue.main.async {
+                    self.repositories = searchResult.items
+                    self.tableView.reloadData()
+                }
+            } catch {
+                print("Decoding error JSON: \(error.localizedDescription)")
+            }
+        }
+        task.resume()
+    }
+}
+
+
+struct SearchResult: Codable {
+    let items: [Repository]
+}
+
+struct Repository: Codable {
+    let name: String
+    let description: String?
+}
+
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return repositories.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let repository = repositories[indexPath.row]
+        cell.textLabel?.text = repository.name
+        cell.detailTextLabel?.text = repository.description ?? "No description available"
+        return cell
+    }
+}
