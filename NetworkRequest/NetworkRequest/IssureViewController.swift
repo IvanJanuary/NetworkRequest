a5811979d6f5a34b9b0a813e01054c5095fece3c
Ivan <>
2023-08-19 14:14:00 +0300

    Create IssureViewController
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchBar: UISearchBar!
    
    Copy and Replace code from RepositoryViewController to IssureViewController

--- NetworkRequest/NetworkRequest/IssureViewController.swift
+++ NetworkRequest/NetworkRequest/IssureViewController.swift
@@ -7,23 +7,71 @@
 
 import UIKit
 
-class IssureViewController: UIViewController {
+class IssureViewController: UIViewController, UISearchBarDelegate {
+   
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    private var repositories: [Repository] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        searchBar.delegate = self
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+        
+    @objc func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let searchText = searchBar.text else { return }
+        searchRepositories(withQuery: searchText)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func searchRepositories(withQuery query: String) {
+        guard !query.isEmpty else {
+            repositories.removeAll()
+            tableView.reloadData()
+            return
+        }
+        
+        let api = "https://api.github.com/search/repositories?q=\(query)"
+        guard let url = URL(string: api) else { fatalError("some Error") }
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            if let error = error {
+                print("Ошибка запроса: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let data = data else { return }
+            
+            do {
+                let decoder = JSONDecoder()
+                let searchResult = try decoder.decode(SearchResult.self, from: data)
+                DispatchQueue.main.async {
+                    self.repositories = searchResult.items
+                    self.tableView.reloadData()
+                }
+            } catch {
+                print("Decoding error JSON: \(error)")
+            }
+        }
+        task.resume()
     }
-    */
-
 }
+
+extension IssureViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return repositories.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let repository = repositories[indexPath.row]
+        cell.textLabel?.text = repository.name
+        cell.detailTextLabel?.text = repository.description ?? "No description available"
+        return cell
+    }
+}
+
