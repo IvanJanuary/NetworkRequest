0187a5360c166976f4b47f00d98c509e77beedb6
Ivan <>
2023-11-03 15:11:18 +0200

    1. Change the bottom constraint in the table in all three view controllers to a value of -40 2. Create a More button in all three view controllers 3. Create a global buttonMore variable in all three view controllers 4. Create constraints for the More button 5. Add the argument page=1 to the link let api = "https://api.github.com/search/commits?page=1&per_page=10&q=\(query)" in all three view controllers. 6. Add the argument page type String to the searchRepositories function in all three view controllers. Add the argument page and value “1” to the searchBarSearchButtonClicked function in all three view controllers. 7. Create a global variable page with the value 1 and convert String to Int in all three view controllers 8. By clicking the button, we add the next page 9. Check searchBar.text for nil We assign the page value 1. When a new request is made, a new page will be loaded Clearing the array Adding new data to the array in all three view controllers

--- NetworkRequest/NetworkRequest/Controller/CommitViewController.swift
+++ NetworkRequest/NetworkRequest/Controller/CommitViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class CommitViewController: UIViewController, UISearchBarDelegate {
+    
+    var buttonMore = UIButton()
+    var searchText: String = ""
+    var page = 1
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchBar: UISearchBar!
@@ -21,27 +25,60 @@ class CommitViewController: UIViewController, UISearchBarDelegate {
         searchBar.delegate = self
         tableView.dataSource = self
         tableView.delegate = self
+        
+        buttonMore = UIButton(type: .roundedRect)
+        buttonMore.frame = CGRect(x: 0, y: 0, width: 64, height: 35)
+        buttonMore.backgroundColor = .systemTeal
+        buttonMore.layer.cornerRadius = 5
+        buttonMore.translatesAutoresizingMaskIntoConstraints = false
+        buttonMore.setTitle("More", for: .normal)
+        buttonMore.setTitleColor(UIColor.white, for: .normal)
+    
+        buttonMore.addTarget(self, action: #selector(pressButton), for: .touchUpInside)
+        self.view.addSubview(buttonMore)
+        
+        buttonMoreConstraint()
+    }
+    
+    func buttonMoreConstraint() {
+        //buttonMore.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 150).isActive = true
+        buttonMore.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1/4).isActive = true
+        buttonMore.heightAnchor.constraint(equalToConstant: 35).isActive = true
+        buttonMore.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        buttonMore.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -80).isActive = true
+    }
+    
+    @objc func pressButton(sender: UIButton) {
+        print("Button is pressed")
+        page += 1
+        searchCommits(withQuery: searchText, page: "\(page)", type: CommitSearchResult.self)
     }
 
     @objc func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        guard let searchText = searchBar.text else { return }
-        searchCommits(withQuery: searchText, type: CommitSearchResult.self) //type: CommitSearchResult.self
+        guard let searchBarText = searchBar.text else {
+            return
+        }
+        page = 1
+        commits = []
+        searchText = searchBarText
+        
+        searchCommits(withQuery: searchText, page: "\(page)", type: CommitSearchResult.self)
     }
     
-    private func searchCommits<T: SearchResult>(withQuery query: String, type: T.Type) { //<T: SearchResult>  type: T.Type
+    private func searchCommits<T: SearchResult>(withQuery query: String, page: String, type: T.Type) {
         guard !query.isEmpty else {
             commits.removeAll()
             tableView.reloadData()
             return
         }
         
-        let api = "https://api.github.com/search/commits?per_page=10&q=\(query)"
+        let api = "https://api.github.com/search/commits?page=1&per_page=10&q=\(query)"
         let helper = GitHubHelper()
         helper.search(withQuery: api, type: CommitSearchResult.self) { [weak self] searchItems in
             guard let self = self else {
                 return
             }
-            self.commits = searchItems as? [CommitItem] ?? [] 
+            self.commits.append(contentsOf: searchItems as? [CommitItem] ?? [])
             self.tableView.reloadData()
         }
     }
--- NetworkRequest/NetworkRequest/Controller/RepositoryViewController.swift
+++ NetworkRequest/NetworkRequest/Controller/RepositoryViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class RepositoryViewController: UIViewController, UISearchBarDelegate {
+    
+    var buttonMore = UIButton()
+    var searchText: String = ""
+    var page = 1
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchBar: UISearchBar!
@@ -20,27 +24,60 @@ class RepositoryViewController: UIViewController, UISearchBarDelegate {
         searchBar.delegate = self
         tableView.dataSource = self
         tableView.delegate = self
-    }
         
-    @objc func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        guard let searchText = searchBar.text else { return }
-        searchRepositories(withQuery: searchText, type: RepositorySearchResult.self) //type: RepositorySearchResult.self
+        buttonMore = UIButton(type: .roundedRect)
+        buttonMore.frame = CGRect(x: 0, y: 0, width: 64, height: 35)
+        buttonMore.backgroundColor = .systemTeal
+        buttonMore.layer.cornerRadius = 5
+        buttonMore.translatesAutoresizingMaskIntoConstraints = false
+        buttonMore.setTitle("More", for: .normal)
+        buttonMore.setTitleColor(UIColor.white, for: .normal)
+    
+        buttonMore.addTarget(self, action: #selector(pressButton), for: .touchUpInside)
+        self.view.addSubview(buttonMore)
+        
+        buttonMoreConstraint()
+    }
+    
+    func buttonMoreConstraint() {
+        //buttonMore.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 150).isActive = true
+        buttonMore.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1/4).isActive = true
+        buttonMore.heightAnchor.constraint(equalToConstant: 35).isActive = true
+        buttonMore.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        buttonMore.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -80).isActive = true
+    }
+    
+    @objc func pressButton(sender: UIButton) {
+        print("Button is pressed")
+        page += 1
+        searchRepositories(withQuery: searchText, page: "\(page)", type: RepositorySearchResult.self)
     }
 
-    private func searchRepositories<T: SearchResult>(withQuery query: String, type: T.Type) { //<T: SearchResult> type: T.Type
+    @objc func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let searchBarText = searchBar.text else {
+            return
+        }
+        page = 1
+        repositories = []
+        searchText = searchBarText
+        
+        searchRepositories(withQuery: searchText, page: "\(page)", type: RepositorySearchResult.self)
+    }
+
+    private func searchRepositories<T: SearchResult>(withQuery query: String, page: String, type: T.Type) {
         guard !query.isEmpty else {
             repositories.removeAll()
             tableView.reloadData()
             return
         }
         
-        let api = "https://api.github.com/search/repositories?per_page=10&q=\(query)"
+        let api = "https://api.github.com/search/repositories?page=1&per_page=10&q=\(query)"
         let helper = GitHubHelper()
         helper.search(withQuery: api, type: RepositorySearchResult.self) { [weak self] searchItems in
             guard let self = self else {
                 return
             }
-        self.repositories = searchItems as? [Repository] ?? []
+            self.repositories.append(contentsOf: searchItems as? [Repository] ?? [])
         self.tableView.reloadData()
         }
     }
